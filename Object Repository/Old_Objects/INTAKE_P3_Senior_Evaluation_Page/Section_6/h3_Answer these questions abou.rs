945166eafd9b9439de8742e8b24a9a6148e7bb53
bAraia <araia.bereket@gmail.com>
2020-03-09 17:50:49 -0400

    test

---
+++ Object Repository/Old_Objects/INTAKE_P3_Senior_Evaluation_Page/Section_6/h3_Answer these questions abou.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_Answer these questions abou</name>
+   <tag></tag>
+   <elementGuidId>6195f9a3-d9ff-4dd7-8b4d-6c9e01e8c2eb</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Answer these questions about the validity of the medical record.</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;section7&quot;)/h3[1]</value>
+   </webElementProperties>
+</WebElementEntity>
