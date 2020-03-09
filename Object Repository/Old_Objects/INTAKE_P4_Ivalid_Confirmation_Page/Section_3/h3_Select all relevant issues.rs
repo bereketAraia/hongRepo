349c8235859565ce27e2f032772b366be887fc88
bAraia <araia.bereket@gmail.com>
2020-03-09 17:47:23 -0400

    test2

---
+++ Object Repository/Old_Objects/INTAKE_P4_Ivalid_Confirmation_Page/Section_3/h3_Select all relevant issues.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_Select all relevant issues</name>
+   <tag></tag>
+   <elementGuidId>0d5a4d4d-b9cb-45af-b1d7-cc984f201fde</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Select all relevant issues with the CMS-generated attestation.</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;section3&quot;)/h3[1]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/RESEARCH_MANAGEMENT/CONTENT/h3_Submission Details.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_Submission Details</name>
+   <tag></tag>
+   <elementGuidId>8fdb196d-a084-4851-9a9b-d77ad9568fd3</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Submission Details</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;mainContent&quot;)/rm-search[1]/section[@class=&quot;container-fluid&quot;]/table[1]/tbody[1]/tr[2]/td[1]/div[1]/div[1]/h3[1]</value>
+   </webElementProperties>
+</WebElementEntity>
